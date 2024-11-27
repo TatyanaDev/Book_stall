78f7fa08795fbfab360186fdb2284474722c380b
TatyanaDev <kot.kate2020@gmail.com>
2024-11-27 16:42:21 +0100

    add jwt-auth

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { JwtStrategy } from '../../core/guards/jwt.strategy';
 import { BooksController } from './books.controller';
 import { BooksRepository } from './books.repository';
+import { UsersModule } from '../users/users.module';
 import { BooksService } from './books.service';
 import { Book } from './book.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book]), UsersModule],
   controllers: [BooksController],
-  providers: [BooksService, BooksRepository],
+  providers: [BooksService, BooksRepository, JwtStrategy],
 })
 export class BooksModule {}
